0868c4356161c79bb1cca0bb2f59829a0c1df8a3
kobaxi <kobaxidzedatka12@gmail.com>
2024-11-08 21:14:36 +0400

    commit

---
+++ level 27/homwork/homwork.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+numbers.append(100)
+numbers.insert(5)
+numbers.remove(30)
+numbers.sort()
+numbers.reverse()
+numbers.index(50)
+numbers.count(20)
+print(numbers)
