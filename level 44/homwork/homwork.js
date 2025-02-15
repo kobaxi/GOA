3cf9fcd2ccd9b4534ffd0a9e958e6e47364af457
kobaxi <kobaxidzedatka12@gmail.com>
2025-02-15 21:07:21 +0400

    commit

---
+++ level 44/homwork/homwork.js
@@ -0,0 +1,52 @@
+// პირველი დავალება
+alert(5 + 3);   
+console.log(5 + 3);  
+
+
+alert(10 - 4);   
+console.log(10 - 4);   
+
+
+alert(7 * 6); 
+console.log(7 * 6); 
+
+
+alert(20 / 4);   
+console.log(20 / 4);  
+
+
+alert(2 ** 3);  
+console.log(2 ** 3);   
+
+
+alert("Hello" + " " + "World");   
+console.log("Hello" + " " + "World");   
+
+alert(15 - 10);   
+console.log(15 - 10);   
+
+
+alert(3 * 4);  
+console.log(3 * 4);   
+
+
+alert(50 / 5);   
+console.log(50 / 5);   
+
+
+alert(4 ** 2);   
+console.log(4 ** 2);  
+//მეორე დავალება
+alert("Hello" + " " + "World");
+console.log("Hello" + " " + "World");
+//მესამე დავალება
+for (let i = 10; i >= 0; i--) {
+    alert("The world will be destroyed in " + i + " seconds");
+    console.log("The world will be destroyed in " + i + " seconds");
+  }
+
+//მეოთხე დავალება მეოთხე მეხუთე და მეექვსე დავალებაა გაერთიანებული
+// alert() - ეს არის ფუნქცია, რომელიც აჩვენებს მცირე პოპ-აპ შეტყობინებას ფანჯარაზე. ის გამოიყენება მომხმარებლისთვის შეტყობინების გადაცემისათვის. 
+alert("Hello World!");
+console.log("This is a log message.");
+// JS (JavaScript) - არის პროგრამირების ენა, რომელიც გამოიყენება ვებგვერდების დინამიური ფუნქციების შესაქმნელად. იგი გამოიყენება HTML და CSS-სთან ერთად, რათა ვებგვერდებს ჰქონდეს ინტერაქტიულობა, ტესტები, ფანჯრები, ფორმების დამუშავება, ან ცოცხალი მონაცემების მანიპულაცია.
