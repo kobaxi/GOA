2904c705e4cfa97d103fe813c843013c46c67b34
kobaxi <kobaxidzedatka12@gmail.com>
2025-02-25 16:43:48 +0400

    commit

---
+++ level 45/classwork/classwork.js
@@ -0,0 +1,6 @@
+const hoursInADay = 24;
+const minutesInAnHour = 60;
+const secondsInAMinute = 60;
+const daysInAMonth = 30;
+const secondsInAMonth = daysInAMonth * hoursInADay * minutesInAnHour * secondsInAMinute;
+console.log(`There are ${secondsInAMonth} seconds in 30 days.`);
---
+++ level 45/homwork/homwork.js
@@ -0,0 +1,19 @@
+// აქ არის პირველი დავალება
+let number1 = 10;
+let number2 = 5;
+const pi = 3.14;
+const e = 2.71;
+// აქ არის გაერთიანებული 2,3,4,5,6 დავალება
+let sum = number1 + number2;
+console.log("ჯამი: " + sum);
+let difference = number1 - number2;
+console.log("სხვაობა: " + difference);
+let product = number1 * number2
+console.log("ნამრავლი: " + product);
+let quotient = number1 / number2;
+console.log("განაყოფი: " + quotient);
+
+// let - ს და const - ს შორის განსხვავება:
+// let შეიძლება შეიცვალოს, ხოლო const-ის მნიშვნელობა ერთჯერ განისაზღვრება და აღარ იცვლება.
+// var განსხვავდება ისევე, როგორც let, მაგრამ var-ის გამოყენება არ არის რეკომენდირებული, რადგან ის შეიძლება იყოს გლობალური
+// რაც შეიძლება გამოიწვიოს პრობლემები დიდი პროექტებში. let არის საუკეთესო არჩევანი, ხოლო const გამოიყენება როცა მნიშვნელობა არ იცვლება.
